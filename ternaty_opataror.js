c1fef67e69d0e4bfa399689f799389ac084168ad
aminulhr <aminulhaquerana1@gmail.com>
2024-09-29 07:58:55 +0600

    ternaty_opataror

---
+++ ternaty_opataror.js
@@ -0,0 +1,13 @@
+var x = 32;
+// if ( result >= 33 ){
+//     console.log('pass the exam')
+// }
+// else{
+
+//     console.log("fail the exam")
+// }
+
+// ternary oparator syntext: condition ; true : false
+
+var result = x >= 33 ? "pass" : "fail";
+console.log(result);
