287f5bc177882b0ceccad9749306994b635cfb46
aminulhr <aminulhaquerana1@gmail.com>
2024-09-29 07:22:41 +0600

    if-else added

--- convartaion.js
+++ convartaion.js
@@ -1,7 +1,5 @@
 var num = 10.22;
-
 console.log(parseInt(num))
-
 var num2 = '20.54';
 console.log(num2)
 console.log(typeof num2)
@@ -9,5 +7,31 @@ console.log(typeof num2)
 // দশমিক তুলতে হলে
 console.log(parseInt(num2))
 
-// স্টি্রং থেকে নাম্বারে কনভার্ট হবে , কিন্তু দশমিক উটবে না।
+// স্টি্রং থেকে নাম্বারে কনভার্ট হবে , কিন্তু দশমিক ও থেকে যাবে।।
+
 console.log(parseFloat(num))
+
+// *************************************************************************
+// দশমিকের পর নির্দিষ্ট নাম্বার নেওয়ার উপায়
+console.log(" Explore দশমিকের পর নির্দিষ্ট নাম্বার নেওয়ার উপায়")
+
+ var rana = 34.2422525
+ console.log(rana)
+  var change = rana.toFixed(1)
+  console.log(change)
+//  toFixed করার পর সেটি স্ট্রিং হয়ে যায়, তাই আবার parseFloat  করে নিতে হবে।
+console.log(parseFloat(change))
+
+
+// ******************************************************************
+console.log('Explorej isNaN')
+// isNaN
+console.log(isNaN(23))
+console.log(isNaN('23'))
+console.log(isNaN('abd'))
+// উপরের ২টাই false  আসবে, কারণ isNaN  চেক করে ২ টা জিনিস
+//             ১. নাম্বার কীনা
+//             ২. নাম্বারে কনভার্ট করা যাবে কীনা
+//  উপরের ২৩ যেহেতু নাম্বার এবং “২৩” কে নাম্বারে কনভার্ট করা যাবে তাই false  আসবে।
+// আর যদি নাম্বার না হয় , এবং নাম্বারে কনভার্ট করাও না যায় তাহলে true দিবে।
+
---
+++ if_else.js
@@ -0,0 +1,13 @@
+var temparature = 20;
+if ( temparature > 30){
+    console.log('It is vary hot day')
+}
+else if  (temparature > 20 ){
+    console.log('It is warm day')
+}
+else if( temparature > 10 ){
+    console.log('it is cool day')
+}
+else{
+    console.log('it is cold day')
+}
