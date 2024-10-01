ad9397c4f3ee4e720c5bff1e8bfbaba04171e8b1
aminulhr <aminulhaquerana1@gmail.com>
2024-10-01 23:51:54 +0600

    problem 1

--- while.js
+++ while.js
@@ -1,5 +1,15 @@
-let i = 1;
-while ( i <= 5) {
-    console.log(i+ " Rana")
-    i++;
+var name = 1;
+while ( name  <= 5) {
+    console.log(name+ " Rana")
+    name++;
 }
+
+// 1 to 10 sum 
+let i = 1;
+let sum = 0;
+while (i <= 10 ){
+    // sum = sum+1;
+    sum+=i;   
+    i++; 
+}
+console.log(sum)
