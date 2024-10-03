fd184dad2388d3de8544f05ca67d094814f0e2b3
aminulhr <aminulhaquerana1@gmail.com>
2024-10-04 01:38:14 +0600

    for loop

--- for.js
+++ for.js
@@ -1,3 +1,8 @@
-for(let i =1; i<=10; i++){
-    console.log(i ,' Rana')
-}
+    // for(let i =1; i<=10; i++){
+    //     console.log(i ,' Rana')
+    // }
+
+    const nums = [23,25,6,5,89,89,89,75];
+    for ( let i = 0; i < nums.length; i++){
+        console.log(nums[i])
+    }
