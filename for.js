5694c5904308923c8fda604fb9ab35c0f95c0816
aminulhr <aminulhaquerana1@gmail.com>
2024-10-04 22:21:54 +0600

    do while

--- for.js
+++ for.js
@@ -9,10 +9,18 @@
 
     // for ( let i = 0; i < nums.length; i++){
     //     console.log(nums[i])
-    // } or 
+    // } 
+
+    // 2 or 
 
     for ( let i = 0; i <= nums.length-2; i++){
         console.log(nums[i])
+    }
+    
+    // 3 or 
+    console.log('get item in variable')
+    for ( item of nums){
+        console.log(item)
     }
 
     // উপরেরটাতে i এর সমান না ধরে i এর থেকে কম নেওয়া হয়েছে, তাই ১ বিয়োগ করা হয়নি।  নিচেরটাতে i এর সমান বা কম নেওয়া হয়েছে তাই ১ বিয়োগ েকরা হলো, 1 মাইনাস না করলে ১টি ঘর আনডিফাইন আসবে।
@@ -22,6 +30,10 @@
 const num2 = [54,45,78,65,92,12,35];
 let sumofodd = 0;
 let sumoeven = 0;
+
+for ( sumItem of num2){
+    console.log(sumItem)
+}
 
 for ( let i= 0; i < num2.length; i++){
     const x = num2[i];
--- while.js
+++ while.js
@@ -13,3 +13,22 @@ while (i <= 10 ){
     i++; 
 }
 console.log(sum)
+let nums = 14;
+while(nums< 16 ){
+    console.log("gratter")
+    nums++;
+}
+let nums2 = 14;
+while(nums2> 19 ){
+    console.log("gratter")
+    nums2++;
+}
+
+console.log('do while loop')
+
+do{
+    console.log(nums2);
+    nums2++;
+}
+while(nums2 >20);
+// do while লুপে মাস্ট লুপটি একবার চলবে , তারপর কন্ডিশন চেক করবে। আর while লুপে আগে কন্ডিশন চেক করবে। তারপর রান হবে।
