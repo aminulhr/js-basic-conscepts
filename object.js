db72ee25175f73bc129134da0b85f55c1ed06f28
aminulhr <aminulhaquerana1@gmail.com>
2024-10-05 11:50:18 +0600

    basic object

---
+++ object.js
@@ -0,0 +1,61 @@
+// JS object syntax
+// const objectName :{
+//     propertyName : PropertyValue;
+// }
+const myBio = {
+    name : "Aminul Haque Rana",
+    age : 23,
+    isMale: true,
+    color: 'Black',
+    isMarrid: false,
+    
+}
+console.log(myBio)
+console.log(myBio.age)
+let isGroom = myBio.isMarrid;
+console.log(isGroom)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
